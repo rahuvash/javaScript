1051af204605aba743ecf9ce3d32a05e7342085d
Rahul Vashisth <rahulvashisth052@gmail.com>
2023-10-28 14:33:09 +0530

    Type Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = null;
+
+console.log(typeof score);
+
+// const {score} = req.body;
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+//typeof(NaN)==> number
+// console.log(typeof valueInNumber);
