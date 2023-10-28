e88d55141a2205efab7e6be92f2e5fac969d9983
Rahul Vashisth <rahulvashisth052@gmail.com>
2023-10-28 20:50:08 +0530

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,52 @@
 let score = null;
 
-console.log(typeof score);
+// console.log(typeof score);
 
 // const {score} = req.body;
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
-
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //typeof(NaN)==> number
 // console.log(typeof valueInNumber);
+
+/*
+Notes after type conversion to number
+"33"=> 33
+"33abc"=>NaN
+null=>0
+true=>1
+*/
+
+let isLoggedIn = 1;
+let boolenaLoggedIn = Boolean(isLoggedIn);
+// console.log(boolenaLoggedIn);
+// output true
+
+// 1=>true; 0=>false
+// ""=> false
+// "rahul"=>true
+
+/***************Operation**********/
+
+let value = 3;
+let negVal = -value;
+console.log(negVal);
+
+// console.table([2+2,2-2,2*2,2**2,2/3,2%3])
+
+let str1 = "1 ";
+let str2 = "Rahul";
+let str3 = str1 + str2;
+console.log(str3);
+
+/*the auotmatic type conversion in javascript is called coercion */
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
