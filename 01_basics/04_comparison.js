d8c54a7490670c424ccdb6ab82d0f5bab242fb11
Rahul Vashisth <rahulvashisth052@gmail.com>
2023-10-28 21:28:57 +0530

    update interview

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,9 +1,9 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null>1)
-console.log(null==0)
-console.log(null>=0)
+console.log(null > 1);
+console.log(null == 0);
+console.log(null >= 0);
 
 /*
     The reason is that an equality check == and comparisons > ,<
@@ -11,3 +11,10 @@ console.log(null>=0)
     comaprisons convert null to a number,treating it as 0
     that is why null>=0 is true
 */
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// all returns false
+
+/* == to compare and === to strictly check with data type comparison*/
---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,46 @@
+/*
+There are two type of data type
+ 1.primitive
+ 2.Non primitive or refrence type
+
+    Primitive (7 type) they are call by value
+    1.String
+    2.Number
+    3.Boolean
+    4.null
+    5.Undefined
+    6.Symbol (use to make a value unique)
+    7.BigInt
+
+    Non Primitive Data type or refrence type
+    1.Array
+    2.Objects
+    3.Functions
+
+ */
+
+/*
+    JavaScript is statically typed language or Dynamically typed language
+    dynamically typed language
+    JavaScript is a dynamically typed language, which means that data types of variables are determined by the value 
+    they hold at runtime and can change throughout the program as we assign different values to them
+*/
+
+/*
+    Non Primitive data type:-
+    1.Arrays
+    const heros= ["shaktiman","nagraj",'doga]
+
+    2.Object
+    let myObj =  {
+        name:"rahul",
+        age:22,
+        date:22/7/2023
+    }
+
+    3. Function
+    const myFunction=()=>{
+        console.log("sorry shaktiman")
+    }
+
+*/
