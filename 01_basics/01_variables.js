8a98f9dcce818f234512cb9bea492399f9f0725c
Rahul Vashisth <rahulvashisth052@gmail.com>
2023-10-28 13:51:54 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+//variable are used for allocating the memory for the data in memory
+const accountId= 144553;
+let accountEmail="rahul@gmail.com";
+var accountPassword="12345";
+accountCity="Faridabad"; //dont use this
+let accountState; // returns undefined
+
+// accountId=2// Not Allowed
+
+console.log(accountId);
+
+accountEmail='rv@rvcom';
+accountPassword='12121';
+accountCity="Delhi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+// This is scope
+// {
+
+// }
+
+/* Var dont understand the concept of block scope & functional scope
+     use only const or var for assigning variable
+ */
