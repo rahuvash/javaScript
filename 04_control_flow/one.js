16760103d4177e005db1cc209c2d961aab85605d
Rahul Vashisth <rahulvashisth052@gmail.com>
2024-01-04 23:47:41 +0530

    minor updates

---
+++ 04_control_flow/one.js
@@ -0,0 +1,9 @@
+// if
+const isUserLoggedIn = true;
+if (isUserLoggedIn) {
+}
+
+//falsey value 0,-0,BiigInt 0n,null, NaN, undefined
+
+//Null Coalesing Operator (??) null undefined
+val1 = 5 ?? 10;
